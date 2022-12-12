3234498f5f6f7b59f29bc3effaaf00bd685aae0a
lukaTarimana <104419299+lukaTarimana@users.noreply.github.com>
2022-12-12 15:43:16 +0400

    fix: some bugs

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -1,9 +1,9 @@
-import { clusterApiUrl ,PublicKey } from "@solana/web3.js";
-import ticktock from './ticktock_Clone.json'
+import { clusterApiUrl, PublicKey } from "@solana/web3.js";
+import ticktock from "./ticktock_Clone.json";
 
-export const SOLANA_HOST = clusterApiUrl('devnet')
+export const SOLANA_HOST = clusterApiUrl("devnet");
 
 export const TICKTOCK_PROGRAM_ID = new PublicKey(
-    "14sBpgJSmzMo5DzJESGZqdvNyaRTA1uSvBt4at1tEJm2"
-)
-export const TICKTOCK_IDL = ticktock
+  "3UUjfr2vkvTCE7g678sSQozrb8iuoHMs6yQQZcJw95Z2"
+);
+export const TICKTOCK_IDL = ticktock;
